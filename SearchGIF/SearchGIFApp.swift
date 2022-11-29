37606fec42a0f82352b13520d0a69c2b57b4451e
vdzjubina <82341003+vdzjubina@users.noreply.github.com>
2022-11-29 17:45:17 +0200

    Initial Commit

---
+++ SearchGIF/SearchGIFApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchGIFApp.swift
+//  SearchGIF
+//
+//  Created by Viktorija on 29/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SearchGIFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
